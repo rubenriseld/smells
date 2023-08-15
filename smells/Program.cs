3243d6b3c0061acfa4a888ae76bc121c00a56e20
rubenriseld <rubenriseld@outlook.com>
2023-08-15 10:11:43 +0200

    code missing fix

--- smells/Program.cs
+++ smells/Program.cs
@@ -1,136 +1,149 @@
-﻿bool continuePlaying = true; //fortsätt spela
-Console.WriteLine("Enter your user name:\n");
-string userName = Console.ReadLine();
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
-while (continuePlaying)
-{
-	string numbersToGuess = GenerateNumbersToGuess(); //skapa siffra som ska gissas
-	Console.WriteLine("New game: \n");
-	Console.WriteLine("For practice, number is: " + numbersToGuess + "\n");
-	string userGuess = Console.ReadLine();
+namespace MooGame;
 
-	int numberOfGuesses = 1;
-	string currentGuessResult = HandleUserGuess(numbersToGuess, userGuess);
-	Console.WriteLine(currentGuessResult+ "\n");
+class MainClass
+{
+    public static void Main(string[] args)
+    {
 
-	string correctGuessResult = "BBBB,";
-	while (currentGuessResult != correctGuessResult)
-	{
-		numberOfGuesses++;
-		userGuess = Console.ReadLine();
-		Console.WriteLine(userGuess + "\n");
-		currentGuessResult = HandleUserGuess(numbersToGuess, userGuess);
-		Console.WriteLine(currentGuessResult + "\n");
-	}
-	StreamWriter output = new StreamWriter("HighScores.txt", append: true);
-	output.WriteLine(userName + "#&#" + numberOfGuesses);
-	output.Close();
-	ShowHighScores();
-	Console.WriteLine("Correct, it took " + numberOfGuesses + " guesses\nContinue?");
-	string answer = Console.ReadLine();
-	if (answer != null && answer != "" && answer.Substring(0, 1) == "n")
-	{
-		continuePlaying = false;
-	}
-}
+        bool continuePlaying = true; //fortsätt spela
+        Console.WriteLine("Enter your user name:\n");
+        string userName = Console.ReadLine();
 
-static string GenerateNumbersToGuess() //siffran som ska gissas
-{
-	Random numberGenerator = new Random();
-	string numbersToGuess = "";
-	for (int i = 0; i < 4; i++)
-	{
-		int newNumber = numberGenerator.Next(10); //upp till 10??
-		string newNumberValue = "" + newNumber; //för att kolla om samma siffra redan valts
-		while (numbersToGuess.Contains(newNumberValue)) //så länge random siffran redan finns hämta ny siffra
-		{
-			newNumber = numberGenerator.Next(10);
-			newNumberValue = "" + newNumber;
-		}
-		numbersToGuess = numbersToGuess + newNumberValue; //utöka goal me ny siffra
-	}
-	return numbersToGuess;
-}
+        while (continuePlaying)
+        {
+            string numbersToGuess = GenerateNumbersToGuess(); //skapa siffra som ska gissas
+            Console.WriteLine("New game: \n");
+            Console.WriteLine("For practice, number is: " + numbersToGuess + "\n");
+            string userGuess = Console.ReadLine();
 
-static string HandleUserGuess(string numbersToGuess, string userGuess)
-{
-	int numberOfBulls = 0;
-	int numberOfCows = 0;
-	userGuess += "    "; //if player entered less than 4 chars
-	for (int correctNumber = 0; correctNumber < 4; correctNumber++)
-	{
-		for (int guessedNumber = 0; guessedNumber < 4; guessedNumber++)
-		{
-			if (numbersToGuess[correctNumber] == userGuess[guessedNumber])
-			{
-				if (correctNumber == guessedNumber)
-				{
-					numberOfBulls++;
-				}
-				else
-				{
-					numberOfCows++;
-				}
-			}
-		}
-	}
-	return "BBBB".Substring(0, numberOfBulls) + "," + "CCCC".Substring(0, numberOfCows);
-}
-static void ShowHighScores()
-{
-	StreamReader streamReader = new StreamReader("HighScores.txt"); //hämta data från textfil me "scores"
-	List<PlayerData> playerHighScores = new List<PlayerData>();
-	string line; //
-	while ((line = streamReader.ReadLine()) != null)
-	{
-		string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None);
-		string name = nameAndScore[0];
-		int score = Convert.ToInt32(nameAndScore[1]);
-		PlayerData playerData = new PlayerData(name, score);
-		int position = playerHighScores.IndexOf(playerData);
-		if (position <0)
-		{
-			playerHighScores.Add(playerData);
-		}
-		else
-		{
-			playerHighScores[position].Update(score);
-		}
-	}
-	playerHighScores.Sort((player1, player2) => player1.Avarage().CompareTo(player2.Avarage()));
-	Console.WriteLine("Player   games avarage");
-	foreach (PlayerData player in playerHighScores)
-	{
-		Console.WriteLine(string.Format("{0, -9}{1,5:D}{2,9:F2}", player.Name, player.NumberOfGames, player.Avarage()));
-	}
-	streamReader.Close();
-}
-class PlayerData
-{
-	public string Name { get; private set; }
-	public int NumberOfGames { get; private set; }
-	int totalGuesses;
-	public PlayerData(string name, int guesses)
-	{
-		Name=name;
-		NumberOfGames=1;
-		this.totalGuesses= guesses;
-	}
-	public void Update(int guesses)
-	{
-		totalGuesses += guesses;
-		NumberOfGames++;
-	}
-	public double Avarage()
-	{
-		return (double)totalGuesses/ NumberOfGames;
-	}
-	//public override bool Equals(Object p)
-	//{
-	//	return Name.Equals(((PlayerData)p).Name);
-	//}
-	//public override int GetHashCode()
-	//{
-	//	return Name.GetHashCode();
-	//}
-}
+            int numberOfGuesses = 1;
+            string currentGuessResult = HandleUserGuess(numbersToGuess, userGuess);
+            Console.WriteLine(currentGuessResult + "\n");
+
+            string correctGuessResult = "BBBB,";
+            while (currentGuessResult != correctGuessResult)
+            {
+                numberOfGuesses++;
+                userGuess = Console.ReadLine();
+                Console.WriteLine(userGuess + "\n");
+                currentGuessResult = HandleUserGuess(numbersToGuess, userGuess);
+                Console.WriteLine(currentGuessResult + "\n");
+            }
+            StreamWriter output = new StreamWriter("HighScores.txt", append: true);
+            output.WriteLine(userName + "#&#" + numberOfGuesses);
+            output.Close();
+            ShowHighScores();
+            Console.WriteLine("Correct, it took " + numberOfGuesses + " guesses\nContinue?");
+            string answer = Console.ReadLine();
+            if (answer != null && answer != "" && answer.Substring(0, 1) == "n")
+            {
+                continuePlaying = false;
+            }
+        }
+
+        static string GenerateNumbersToGuess() //siffran som ska gissas
+        {
+            Random numberGenerator = new Random();
+            string numbersToGuess = "";
+            for (int i = 0; i < 4; i++)
+            {
+                int newNumber = numberGenerator.Next(10); //upp till 10??
+                string newNumberValue = "" + newNumber; //för att kolla om samma siffra redan valts
+                while (numbersToGuess.Contains(newNumberValue)) //så länge random siffran redan finns hämta ny siffra
+                {
+                    newNumber = numberGenerator.Next(10);
+                    newNumberValue = "" + newNumber;
+                }
+                numbersToGuess = numbersToGuess + newNumberValue; //utöka goal me ny siffra
+            }
+            return numbersToGuess;
+        }
+
+        static string HandleUserGuess(string numbersToGuess, string userGuess)
+        {
+            int numberOfBulls = 0;
+            int numberOfCows = 0;
+            userGuess += "    "; //if player entered less than 4 chars
+            for (int correctNumber = 0; correctNumber < 4; correctNumber++)
+            {
+                for (int guessedNumber = 0; guessedNumber < 4; guessedNumber++)
+                {
+                    if (numbersToGuess[correctNumber] == userGuess[guessedNumber])
+                    {
+                        if (correctNumber == guessedNumber)
+                        {
+                            numberOfBulls++;
+                        }
+                        else
+                        {
+                            numberOfCows++;
+                        }
+                    }
+                }
+            }
+            return "BBBB".Substring(0, numberOfBulls) + "," + "CCCC".Substring(0, numberOfCows);
+        }
+        static void ShowHighScores()
+        {
+            StreamReader streamReader = new StreamReader("HighScores.txt"); //hämta data från textfil me "scores"
+            List<PlayerData> playerHighScores = new List<PlayerData>();
+            string line; //
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None);
+                string name = nameAndScore[0];
+                int score = Convert.ToInt32(nameAndScore[1]);
+                PlayerData playerData = new PlayerData(name, score);
+                int position = playerHighScores.IndexOf(playerData);
+                if (position < 0)
+                {
+                    playerHighScores.Add(playerData);
+                }
+                else
+                {
+                    playerHighScores[position].Update(score);
+                }
+            }
+            playerHighScores.Sort((player1, player2) => player1.Avarage().CompareTo(player2.Avarage()));
+            Console.WriteLine("Player   games avarage");
+            foreach (PlayerData player in playerHighScores)
+            {
+                Console.WriteLine(string.Format("{0, -9}{1,5:D}{2,9:F2}", player.Name, player.NumberOfGames, player.Avarage()));
+            }
+            streamReader.Close();
+            }
+        }
+        class PlayerData
+        {
+            public string Name { get; private set; }
+            public int NumberOfGames { get; private set; }
+            int totalGuesses;
+            public PlayerData(string name, int guesses)
+            {
+                Name = name;
+                NumberOfGames = 1;
+                this.totalGuesses = guesses;
+            }
+            public void Update(int guesses)
+            {
+                totalGuesses += guesses;
+                NumberOfGames++;
+            }
+            public double Avarage()
+            {
+                return (double)totalGuesses / NumberOfGames;
+            }
+            //public override bool Equals(Object p)
+            //{
+            //	return Name.Equals(((PlayerData)p).Name);
+            //}
+            //public override int GetHashCode()
+            //{
+            //	return Name.GetHashCode();
+            //}
+        }
+    }
