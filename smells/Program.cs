b28ea008d7ea7d191edc8dbad99427195c1d1d9d
rubenriseld <rubenriseld@outlook.com>
2023-08-28 10:59:32 +0200

    merge fix in program.cs

--- smells/Program.cs
+++ smells/Program.cs
@@ -12,13 +12,8 @@ class MainClass
 	{
         IUI consoleUserInterface = new ConsoleUI();
 
-		IUI userInterface = new UI(); 
 		CowsAndBulls cowsAndBulls = new CowsAndBulls();
-		GameController gameController = new GameController();
-		gameController.AddUserInterface(userInterface).AddGame(cowsAndBulls);
-		gameController.Menu();
-
-        GameController gameController = new GameController(consoleUserInterface);
+		GameController gameController = new GameController(consoleUserInterface);
 
 		gameController
 			.AddGame(cowsAndBulls)
